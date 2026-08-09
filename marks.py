de3cf8112c20dec915fc7c4d116e6101c11d6405
mundeakashay1212-dotcom <mundeakashay1212@gmail.com>
2026-08-09 17:59:37 +0530

    Add files via upload

--- marks.py
+++ marks.py
@@ -1,6 +1,32 @@
+# Student Marks Program
 
+name = input("Enter student name: ")
 
-ms")
+maths = float(input("Enter Maths marks: "))
+science = float(input("Enter Science marks: "))
+english = float(input("Enter English marks: "))
+social = float(input("Enter Social Science marks: "))
+computer = float(input("Enter Computer marks: "))
+
+total = maths + science + english + social + computer
+percentage = total / 5
+
+print("\n--- Student Result ---")
+print("Name:", name)
+print("Total Marks:", total)
+print("Percentage:", percentage, "%")
+
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+elif percentage >= 50:
+    grade = "D"
 else:
-    print("Result: Fail")
-    
+    grade = "F"
+
+print("Grade:", grade)
