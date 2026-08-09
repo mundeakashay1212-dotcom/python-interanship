2ab5da604df68c07d5059074fcdef8f68e46fbb2
mundeakashay1212-dotcom <mundeakashay1212@gmail.com>
2026-08-09 18:39:48 +0530

    Add files via upload

---
+++ static.py
@@ -0,0 +1,16 @@
+# Static Calculator Program
+
+num1 = 20
+num2 = 10
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("First Number:", num1)
+print("Second Number:", num2)
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
